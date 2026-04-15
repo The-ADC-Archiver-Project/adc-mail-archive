e7aaee653a5f3f56b152a54ef76fce368d36a9f6
Mealman1551 <nathandubuy4@gmail.com>
2026-04-15 21:16:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-thread.items.forEach(item => {
-  html += `
-    <div style="margin-bottom:10px;">
-      <h4>
-        ${item.title}
-        <a href="${item.link}" target="_blank" style="margin-left:8px;">
-          📧
-        </a>
-      </h4>
+fetch('./feed.json')
+  .then(r => r.json())
+  .then(data => {
+    const app = document.getElementById("app")
 
-      <pre style="white-space: pre-wrap">${item.content}</pre>
-      <hr>
-    </div>
-  `
-})
+    data.forEach(thread => {
+      let html = ""
+
+      thread.items.forEach(item => {
+        html += `
+          <div style="margin-bottom:10px;">
+            <h4>
+              ${item.title}
+              <a href="${item.link}" target="_blank" style="margin-left:8px;">
+                📧
+              </a>
+            </h4>
+
+            <pre style="white-space: pre-wrap">${item.content}</pre>
+            <hr>
+          </div>
+        `
+      })
+
+      const div = document.createElement("div")
+      div.innerHTML = `
+        <h2>${thread.thread}</h2>
+        ${html}
+      `
+
+      app.appendChild(div)
+    })
+  })
